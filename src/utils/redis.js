d407dbda578c25b27911b04abf5083a7f58367b1
Manaal <parikhmanaal@gmail.com>
2026-05-31 05:23:11 +0530

    keep redis alive

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,7 +1,10 @@
 const { createClient } = require("redis");
 const { REDIS_URL } = require("../config");
 
-const redisClient = createClient({ url: REDIS_URL });
+const redisClient = createClient({
+  url: REDIS_URL,
+  pingInterval: 60000 // Send a background PING every 60 seconds to keep the socket alive
+});
 
 redisClient.on("error", (err) => console.error("❌ Redis Client Error:", err));
 redisClient.on("connect", () => console.log("✅  Redis client connected"));
